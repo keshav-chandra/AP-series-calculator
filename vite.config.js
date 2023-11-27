ef5c4a876e7b8a2a89605edb26b8b55d732ec7b7
keshav-chandra <keshavchandra2222@gmail.com>
2023-11-27 12:48:44 +0530

    deployed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/arithmetic-series/',
   plugins: [react()],
   base: '/vite-deploy-demo/',
 })
